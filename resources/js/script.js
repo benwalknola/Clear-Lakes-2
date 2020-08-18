60ccd948c303f406fac58728962c520a4efac3fa
benwalknola <benwalknola@yahoo.com>
2020-08-17 22:08:18 -0700

    Finished adding the image slider functionality!

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,19 @@
 let slides = document.querySelectorAll('.slide');
+let slides2 = document.querySelectorAll('.slide-2')
 const next = document.querySelector('#next');
 const prev = document.querySelector('#prev');
+const next2 = document.querySelector('#next-2');
+const prev2 = document.querySelector('#prev-2');
 const auto = true;
 const intervalTime = 6000;
+const intervalTime2 = 5500;
 let slideInterval;
+let slideInterval2;
 
 let nextSlide = () => {
     const current = document.querySelector('.current');
     current.classList.remove('current')
-    if(current.nextElementSibling) {
+    if (current.nextElementSibling) {
         // Add current to the next sibling
         current.nextElementSibling.classList.add('current')
     } else {
@@ -26,8 +31,33 @@ let prevSlide = () => {
     } else {
         slides[slides.length - 1].classList.add('current');
     }
-    setTimeout(() => current.classList.remove('current'));
+    setTimeout(() => current.classList.remove('current-2'));
 }
+
+let nextSlide2 = () => {
+    const current2 = document.querySelector('.current-2');
+    current2.classList.remove('current-2')
+
+    if(current2.nextElementSibling) {
+        current2.nextElementSibling.classList.add('current-2')
+    } else {
+        slides2[0].classList.add('current-2');
+    }
+    setTimeout(() => current2.classList.remove('current-2'));
+}
+
+
+let prevSlide2 = () => {
+    const current2 = document.querySelector('current-2');
+    current2.classList.remove('current-2')
+    if (current2.previousElementSibling) {
+        current2.previousElementSibling.classList.add('current-2') 
+    } else {
+        slides2[slides2.length - 1].classList.add('.current-2')
+    }
+    setTimeout(() => current2.classList.remove('current-2'));
+}
+
 
 next.addEventListener('click', e => {
     nextSlide();
@@ -36,6 +66,7 @@ next.addEventListener('click', e => {
         slideInterval = setInterval(nextSlide, intervalTime);
     }
 });
+
 prev.addEventListener('click', e => {
     prevSlide();
     if (auto) {
@@ -44,9 +75,26 @@ prev.addEventListener('click', e => {
     }
 });
 
+next2.addEventListener('click', e => {
+    nextSlide2();
+    if (auto) {
+        clearInterval(slideInterval2);
+        slideInterval2 = setInterval(nextSlide2, intervalTime2);
+    }
+});
+
+prev2.addEventListener('click', e => {
+    prevSlide2();
+    if (auto) {
+        clearInterval(slideInterval);
+        slideInterval = setInterval(nextSlide2, intervalTime2);
+    }
+});
+
 // Auto Slide 
 
 if (auto) {
     // run next slide at interval time 
     slideInterval = setInterval(nextSlide, intervalTime)
+    slideInterval2 = setInterval(nextSlide2, intervalTime2)
 }
