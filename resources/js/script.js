712bf705e3d70234329829fd5f5d3e7ba5f0d492
benwalknola <benwalknola@yahoo.com>
2020-08-30 19:25:55 -0700

    Added animations to the elements

--- resources/js/script.js
+++ resources/js/script.js
@@ -97,4 +97,71 @@ if (auto) {
     // run next slide at interval time 
     slideInterval = setInterval(nextSlide, intervalTime)
     slideInterval2 = setInterval(nextSlide2, intervalTime2)
-}
+};
+
+// Animated Section 
+
+$(document).ready(function() { 
+
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '90%'
+    });
+
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInLeft')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeInRight')
+    }, {
+        offset: '70%'
+    });
+
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated fadeInLeft')
+    }, {
+        offset: '70%'
+    });
+
+    $('.js--wp-5').waypoint(function(direction) {
+        $('.js--wp-5').addClass('animated fadeInRight')
+    }, {
+        offset: '70%'
+    });
+
+    $('.js--wp-6').waypoint(function(direction) {
+        $('.js--wp-6').addClass('animated fadeInDown')
+    }, {
+        offset: '80%'
+    });
+
+    $('.js--wp-7').waypoint(function(direction) {
+        $('.js--wp-7').addClass('animated fadeIn')
+    }, {
+        offset: '100%'
+    });
+
+    $('.js--wp-8').waypoint(function(direction) {
+        $('.js--wp-8').addClass('animated fadeInUp')
+    }, {
+        offset: '70%'
+    });
+
+    $('.js--wp-9').waypoint(function(direction) {
+        $('.js--wp-9').addClass('animated fadeInDown')
+    }, {
+        offset: '70%'
+    });
+
+    $('.js--wp-10').waypoint(function(direction) {
+        $('.js--wp-10').addClass('animated fadeIn')
+    }, {
+        offset: '60%'
+    });
+
+});
+
